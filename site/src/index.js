1d40994f6e56058c50a375f21991e2b2eae266ef
jpmesquit4 <joaopedromesq123@gmail.com>
2024-05-25 20:42:30 -0300

    ok

---
+++ site/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import Home from './pages/home';
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
---
+++ site/src/pages/home/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+function Home() {
+  return (
+    <div className="pagina-home">
+      <h1>oi</h1>
+    </div>
+  );
+}
+
+export default Home;
